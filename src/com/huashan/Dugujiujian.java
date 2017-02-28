3000bb5c87e74e3c9ae14aec543662604494b060
岳不群 <892550832@qq.com>
2017-02-28 09:12:57 +0800

    update 02

--- src/com/huashan/Dugujiujian.java
+++ src/com/huashan/Dugujiujian.java
@@ -3,5 +3,8 @@ package com.huashan;
 public class Dugujiujian {
 	public static void main(String[] args) {
 		System.out.println("dugujiujian");
+		System.out.println("dugujiujian");
+		System.out.println("dugujiujian");
+		System.out.println("dugujiujian");
 	}
 }
